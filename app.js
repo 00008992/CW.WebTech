cfa51ed64a6664c32717bd8bc71f1ef9f6d2de86
8992 <Usmanov.azamjan@gmail.com>
2021-04-07 10:34:58 +0500

    styles, create home layout pages were added

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'pug')
+
+app.use('/static', express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/create', (req, res) => {
+    res.render('create')
+})
+
+const posts = ['Some posts', 'Some better posts']
+
+app.get('/posts', (req, res) => {
+  res.render('posts', { posts: posts })
+})
+
+app.get('/posts/detail', (req, res) => {
+    res.render('detail')
+})
+
+app.listen(5000, err => {
+  if (err) console.log(err)
+
+  console.log('Server is running on port 5000...')
+})
